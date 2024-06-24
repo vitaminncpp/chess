47ac6efa6da667f27b95fc8e5e145ea4ab22e7b0
vitaminncpp <akshaykumarparmar51@gmail.com>
2024-06-24 23:20:49 +0530

    Initial commit

---
+++ src/main/java/com/vitaminncpp/chess/ChessApplication.java
@@ -0,0 +1,14 @@
+package com.vitaminncpp.chess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		SpringApplication.run(ChessApplication.class, args);
+	}
+
+}
